796471c3d5f6676963f26b1968af1b1017e5e75f
Kim Dong Hwan <sioil3333.gmail.com>
2023-08-21 03:00:22 +0900

    피연산자 2개 및 연산자 1개로 계산하는 로직 완성.

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +25,6 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view: View){
         tvInput?.append((view as Button).text)
         lastNumeric = true
-        lastDot = false
-
     }
 
     fun onClear(view: View){
@@ -41,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         lastDot = true
     }
 
+
     fun onOperator(view: View){
         tvInput?.text?.let{
             if(lastNumeric && !isOperatorAdded(it.toString())){
@@ -50,6 +51,86 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1);
+                }
+
+                when {
+                    tvValue.contains("/") -> {
+
+                        val splitedValue = tvValue.split("/")
+
+                        var one = splitedValue[0]
+                        val two = splitedValue[1]
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                    }
+                    tvValue.contains("*") -> {
+
+                        val splitedValue = tvValue.split("*")
+
+                        var one = splitedValue[0]
+                        val two = splitedValue[1]
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                    }
+                    tvValue.contains("-") -> {
+
+                        val splitedValue = tvValue.split("-")
+
+                        var one = splitedValue[0]
+                        val two = splitedValue[1]
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                    }
+                    tvValue.contains("+") -> {
+
+                        val splitedValue = tvValue.split("+")
+
+                        var one = splitedValue[0]
+                        val two = splitedValue[1]
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                    }
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String): String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length -2)
+
+        return value
     }
     private fun isOperatorAdded(value: String) : Boolean{
         return if(value.startsWith("-")){
